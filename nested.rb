f3e171373ad982e810c5f6cd8765c9596fd0260f
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 21:31:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,8 +94,6 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-  programmer_hash[:alan_kay][:known_for] = "GUI"
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -110,6 +108,7 @@ def changing_alan
         :known_for => "Unix",
         :languages => ["C"]
       }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
     }
   return programmer_hash
 end
