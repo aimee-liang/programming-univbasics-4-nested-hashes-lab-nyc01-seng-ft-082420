2175ed252dcf0077f6e665bea1e327cec09adceb
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 21:32:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,8 +117,6 @@ def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
   
-  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -134,5 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
   return programmer_hash
 end
