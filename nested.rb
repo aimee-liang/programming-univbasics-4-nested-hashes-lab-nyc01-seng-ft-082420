91e8db71cb1331d8a2be40526c7fae27766c6270
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 20:35:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -93,6 +93,7 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 
 	programmer_hash = 
  		{
@@ -109,7 +110,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  return programmer_hash
 end
 
 def adding_to_dennis
