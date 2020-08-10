180700bc8b068167deb265318d25ac6d12264cdd
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 20:23:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-  programmer_hash = {
+  programmer_hash.merge = {
     :yukihiro_matsumoto => {
       :known_for => "Ruby",
       :languages => ["LISP", "C"]
